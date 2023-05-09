90721dd2ab041d7d44c63ea26fd4a2ce5ef88f70
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-05-09 17:57:59 -0400

    Initial Commit

---
+++ MovieTrack/MovieTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackApp.swift
+//  MovieTrack
+//
+//  Created by Aaron Wilson on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
